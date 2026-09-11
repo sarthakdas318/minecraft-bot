8c25a5bbdbf8f6111a27fb00d4374ff99222a926
surajit <surajitjana20107@gmail.com>
2026-09-11 19:00:31 +0530

    bind server to all network interfaces

--- index.js
+++ index.js
@@ -388,4 +388,4 @@ connectBot();
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT,"0.0.0.0", () => console.log(`Server started on port ${PORT}`));
